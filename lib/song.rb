d744bf31c33497226430c43ac578c44b1b97cc76
Brad Alexander <brad.alexander913@gmail.com>
2019-09-15 23:09:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
 class Song 
-  
+  @@all = []
   attr_accessor :name, :genre
   attr_reader :artist
   
   def initialize(name)
     @name = name
+    @@all << self
   end 
   
   def artist= (name)
